298a815a0b9f87e1c080638b683cc9b8765f0e13
Bravor9 <161014906+Bravor9@users.noreply.github.com>
2024-04-13 17:59:24 +0200

    Prueba de nueva rama
    Ya está hecho el cliente de ecuaciones + funcionalidad resolver. Falta conectarlos

---
+++ WCF_Project/MathService/EquationsClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EquationsClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Generar(object sender, RoutedEventArgs e)
+        {
+            // Verifica si el usuario ha ingresado un número válido de ecuaciones
+            if (int.TryParse(Num_eq.Text, out int numEcuaciones) && numEcuaciones > 0)
+            {
+                // Limpia el grid si ya hay controles en él
+                EsquemaGrid.Children.Clear();
+                EsquemaGrid.RowDefinitions.Clear();
+                EsquemaGrid.ColumnDefinitions.Clear();
+
+                // Añade columnas para las ecuaciones, el signo "=" y las soluciones
+                for (int i = 0; i < numEcuaciones; i++)
+                {
+                    EsquemaGrid.RowDefinitions.Add(new RowDefinition());
+                    EsquemaGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                }
+                EsquemaGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto }); // Columna para el signo "="
+                EsquemaGrid.ColumnDefinitions.Add(new ColumnDefinition()); // Columna para las soluciones
+
+                // Crea el esquema de tamaño (N+2)xN (una columna adicional para el signo "=" y otra para las soluciones)
+                for (int i = 0; i < numEcuaciones; i++)
+                {
+                    for (int j = 0; j < numEcuaciones + 2; j++)
+                    {
+                        if (j == numEcuaciones) // Agrega el signo "=" en la columna adecuada
+                        {
+                            var label = new Label
+                            {
+                                Content = "=",
+                                FontSize = 16,
+                                Margin = new Thickness(1),
+                                VerticalContentAlignment = VerticalAlignment.Center,
+                                HorizontalContentAlignment = HorizontalAlignment.Center
+                            };
+                            Grid.SetRow(label, i);
+                            Grid.SetColumn(label, j);
+                            EsquemaGrid.Children.Add(label);
+                        }
+                        else // Agrega un TextBox para las ecuaciones y las soluciones
+                        {
+                            var textBox = new TextBox
+                            {
+                                FontSize = 16,
+                                Margin = new Thickness(1),
+                                VerticalContentAlignment = VerticalAlignment.Center,
+                                HorizontalContentAlignment = HorizontalAlignment.Center
+                            };
+                            Grid.SetRow(textBox, i);
+                            Grid.SetColumn(textBox, j);
+                            EsquemaGrid.Children.Add(textBox);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, introduce un número válido de ecuaciones.");
+            }
+        }
+
+    }
+}
---
+++ WCF_Project/MathService/MathService/Math.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace MathService
+{
+    public class Math : IMath
+    {
+
+
+        //https://stackoverflow.com/questions/15743192/check-if-number-is-prime-number
+        public bool Prime(int value)
+        {
+
+            if (value == 0 || value == 1)
+            {
+                Console.WriteLine(value + " is not prime number");
+                return false;
+            }
+            else
+            {
+                for (int a = 2; a <= value / 2; a++)
+                {
+                    if (value % a == 0)
+                    {
+                        Console.WriteLine(value + " is not prime number");
+                        return false;
+                    }
+
+                }
+                Console.WriteLine(value + " is a prime number");
+                return true;
+            }
+
+        }
+
+        public Tuple sumaInterna(Tuple tupla)
+        {
+            String name = tupla.Name;
+            double[] resultado = new double[1];
+
+            double sumaTupla = tupla.Data.Sum();
+            resultado[0] = sumaTupla;
+
+            Tuple tuplaFinal = new Tuple();
+            tuplaFinal.Name = $"Suma interna de {name}";
+            tuplaFinal.Data = resultado;
+            return tuplaFinal;
+        }
+
+        public double[] resolverEcuaciones(Tuple tupla)
+        {
+            //Datos
+            // Extraer la matriz de ecuaciones y el vector de datos de la tupla
+            Matrix<double> ecuaciones = tupla.Equation;
+            double[] datos = tupla.Data;
+
+            // Resolver el sistema de ecuaciones
+            Vector<double> resultado = ecuaciones.Solve(Vector<double>.Build.DenseOfArray(datos));
+
+            // Convertir el resultado a un array de doubles
+            double[] soluciones = resultado.ToArray();
+
+            return soluciones;
+
+        }
+
+    }
+}
